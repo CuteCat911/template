1be9fa191e068bc2b9956dd3790db60b1d94d76e
WebCuteCat <webcutecat@gmail.com>
2018-01-13 01:04:55 +0300

    get-associative-array-length ver. 1.0.0

--- dev/babel/general/get-associative-array-length.js
+++ dev/babel/general/get-associative-array-length.js
@@ -1,24 +1,12 @@
 // getAssociativeArrayLength - ver 1.0.0
 
-// Description
-// * * * = * * *
+export let getAssociativeArrayLength = function(associativeArray) {
 
-// Функция возвращающая длинну ассоциативного массива.
-
-// Возвращает число.
-
-// Принимает в себя ассоциативный массив.
-
-// * * * = * * *
-// End Description
-
-export let getAssociativeArrayLength = function(array) {
-
-  if (array && typeof array === "object") {
+  if (associativeArray && typeof associativeArray === "object" && !Array.isArray(associativeArray)) {
 
     let length = 0;
 
-    for (let i in array) {
+    for (let i in associativeArray) {
 
       length++;
 
