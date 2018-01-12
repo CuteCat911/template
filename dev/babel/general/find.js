6b3f29d9ccab2189fa6975973bc5dbdbf247e6e2
WebCuteCat <webcutecat@gmail.com>
2018-01-12 21:42:34 +0300

    find ver. 1.0.0

--- dev/babel/general/find.js
+++ dev/babel/general/find.js
@@ -1,16 +1,4 @@
-// findElemsClass - ver. 1.1.1
-
-// Description
-// * * * = * * *
-
-// Функция для поиска элементов на странице по заданному классу;
-
-// Возвращает массив найденых элементов;
-
-// Принимает в себя класс по которому будет искать элементы (elemsClass) и место где нужно эти элементы искать (lookPlace).
-
-// * * * = * * *
-// End Description
+// findElemsClass - ver. 1.0.0
 
 export let findElemsClass = function(elemsClass, lookPlace) {
 
@@ -33,19 +21,7 @@ export let findElemsClass = function(elemsClass, lookPlace) {
 
 };
 
-// findFirstClass - ver. 1.0.1
-
-// Description
-// * * * = * * *
-
-// Функция для поиска первого элемента на странице по заданному классу;
-
-// Возвращает только один найденый элемент (самый первый который найдет);
-
-// Принимает в себя класс по которому будет искать элемент (elemClass) и место где нужно этот элемент искать (lookPlace).
-
-// * * * = * * *
-// End Description
+// findFirstClass - ver. 1.0.0
 
 export let findFirstClass = function(elemClass, lookPlace) {
 
@@ -68,23 +44,11 @@ export let findFirstClass = function(elemClass, lookPlace) {
 
 };
 
-// findElemsClasses - ver. 1.1.2
-
-// Description
-// * * * = * * *
-
-// Функция для поиска элементов на странице по заданным классам;
-
-// Возвращает массив с ассивами найденых элементов;
-
-// Принимает в себя массив с классами по которым будут искаться элементы (elemsClassesArray) и место где нужно искать элементы (lookPlace).
-
-// * * * = * * *
-// End Description
+// findElemsClasses - ver. 1.0.0
 
 export let findElemsClasses = function(elemsClassesArray, lookPlace) {
 
-	if ((elemsClassesArray && typeof elemsClassesArray === "object") && (lookPlace && typeof lookPlace === "object")) {
+	if ((elemsClassesArray && Array.isArray(elemsClassesArray)) && (lookPlace && typeof lookPlace === "object")) {
 
 		let array = [];
 
@@ -119,19 +83,7 @@ export let findElemsClasses = function(elemsClassesArray, lookPlace) {
 
 };
 
-// findElemsTag - ver. 1.1.1
-
-// Description
-// * * * = * * *
-
-// Функция для поиска элементов на странице по заданному тегу;
-
-// Возвращает массив найденых элементов;
-
-// Принимает в себя тег по которому будет искать элементы (elemsTag) и место где нужно эти элементы искать (lookPlace).
-
-// * * * = * * *
-// End Description
+// findElemsTag - ver. 1.0.0
 
 export let findElemsTag = function(elemsTag, lookPlace) {
 
@@ -154,19 +106,7 @@ export let findElemsTag = function(elemsTag, lookPlace) {
 
 };
 
-// findFirstTag - ver. 1.0.1
-
-// Description
-// * * * = * * *
-
-// Функция для поиска первого элемента на странице по заданному тегу;
-
-// Возвращает только один найденый элемент (самый первый который найдет);
-
-// Принимает в себя тег по которому будет искать элемент (elemTag) и место где нужно этот элемент искать (lookPlace).
-
-// * * * = * * *
-// End Description
+// findFirstTag - ver. 1.0.0
 
 export let findFirstTag = function(elemTag, lookPlace) {
 
@@ -189,23 +129,11 @@ export let findFirstTag = function(elemTag, lookPlace) {
 
 };
 
-// findElemsTags - ver. 1.1.2
-
-// Description
-// * * * = * * *
-
-// Функция для поиска элементов на странице по заданным тегам;
-
-// Возвращает массив с массивами найденых элементов;
-
-// Принимает в себя массив с тегами по которым будут искаться элементы (elemsTagsArray) и место где нужно искать элементы (lookPlace).
-
-// * * * = * * *
-// End Description
+// findElemsTags - ver. 1.0.0
 
 export let findElemsTags = function(elemsTagsArray, lookPlace) {
 
-	if ((elemsTagsArray && typeof elemsTagsArray === "object") && (lookPlace && typeof lookPlace === "object")) {
+	if ((elemsTagsArray && Array.isArray(elemsTagsArray)) && (lookPlace && typeof lookPlace === "object")) {
 
 		let array = [];
 
