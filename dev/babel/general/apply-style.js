982970df21b76c7dc9ffd674681d0a23421094eb
WebCuteCat <webcutecat@gmail.com>
2018-01-13 00:55:00 +0300

    apply-style ver. 1.0.0

--- dev/babel/general/apply-style.js
+++ dev/babel/general/apply-style.js
@@ -1,29 +1,4 @@
-// applyStyle - ver. 1.0.1
-
-// Description
-// * * * = * * *
-
-// Функция для добавления или удаления css стилей у элемента;
-
-// Принимает в себя:
-// 1. Элемент (elem);
-// 2. Объект в котором записаны названия стилей со значениями (style);
-// 3. Событие которое должно быть сделано (event);
-
-// Пример объекта style:
-// style = {
-// 	width: "100px",
-// 	height: "20px",
-// 	backgroundColor: "#fff",
-// 	fontSize: "15px"
-// };
-
-// Возможные значения параметра event:
-// 1. add - добавление стилей;
-// 2. remove - удаление стилей;
-
-// * * * = * * *
-// End Description
+// applyStyle - ver. 1.0.0
 
 export let applyStyle = function(elem, style, event) {
 
