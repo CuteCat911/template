ec2fea762edca07b19d4bd24e04adbb781ca289c
WebCuteCat <webcutecat@gmail.com>
2018-01-13 01:10:37 +0300

    ajax ver. 1.0.0

--- dev/babel/general/ajax.js
+++ dev/babel/general/ajax.js
@@ -1,15 +1,5 @@
 // getXmlHttp - ver. 1.0.0
 
-// Description
-// * * * = * * *
-
-// Функция получения объекта xmlHttp, используемого для обмена данными с сервером.
-
-// Возвращает объект xmlHttp. 
-
-// * * * = * * *
-// End Description
-
 export let getXmlHttp = function() {
 
 	let xmlHttp;
@@ -44,29 +34,6 @@ export let getXmlHttp = function() {
 
 // serialize - ver. 1.0.0
 
-// Description
-// * * * = * * *
-
-// Функция преобразования объекта с данными в корректную строку для передачи этих данных на сервер.
-
-// Принимает в себя тип передачи данных (type) и сами данные (data).
-
-// Type может иметь значения:
-// 1. Get - для небольших запросов (нельзя пересылать пароли).
-// 2. Post - для больших данных.
-
-// Data должен быть объектом, например:
-// {
-//   name: Ivan,
-//   id: 43,
-//   type: type3
-// }
-
-// Возвращает преобразованные данные в виде строки, готовые к отправке на сервер;
-
-// * * * = * * *
-// End Description
-
 export let serialize = function(type, data) {
 
 	if ((type == "POST" || type == "GET") && (data && typeof data === "object")) {
@@ -91,11 +58,6 @@ export let serialize = function(type, data) {
 		}
 
     return serializeData;
-
-	} else {
-
-		console.error();
-		return;
 
 	}
 
