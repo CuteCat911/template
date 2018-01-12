de35cf968e1a067976f5ea04c5c7cd5e98778d5f
WebCuteCat <webcutecat@gmail.com>
2018-01-13 01:06:24 +0300

    inspect-mobile ver. 1.0.0

--- dev/babel/general/inspect-mobile.js
+++ dev/babel/general/inspect-mobile.js
@@ -1,20 +1,10 @@
 // inspectMobile - ver. 1.0.0
 
-// Description
-// * * * = * * *
-
-// Функция для проверки на мобильного типа браузера;
-
-// Возвращает true если браузер на мобильном устройстве или false если десктоп.
-
-// * * * = * * *
-// End Description
-
 export let inspectMobile = function() {
 
 	let mobile = false;
 	let browser = navigator.userAgent;
-	let mobileBrowsers = ["Android","iPhone","iPad","iPod","BlackBerry","Opera Mini","IEMobile"];
+	let mobileBrowsers = ["Android", "iPhone", "iPad", "iPod", "BlackBerry", "Opera Mini", "IEMobile"];
 
 	for (let item of mobileBrowsers) {
 
