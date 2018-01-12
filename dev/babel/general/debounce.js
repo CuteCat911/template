466181df71e6a8386e5e27e3b42319f2a8d04259
WebCuteCat <webcutecat@gmail.com>
2018-01-13 01:19:05 +0300

    debounce ver. 1.0.0

--- dev/babel/general/debounce.js
+++ dev/babel/general/debounce.js
@@ -1,27 +1,11 @@
 // debounce - ver. 1.0.0
 
-// Description
-// * * * = * * *
-
-// Откладывание события на определенное время после первого исполнения.
-
-// Принимает в себя функцию (func) и время задержки (time).
-
-// * * * = * * *
-// End Description
-
 let state = null;
 const FROZE = 1;
 
-export let debounce = function(func, time) {
+export let debounce = function(func, time = 300) {
 
 	if (func && typeof func === "function") {
-
-		if (time == undefined && typeof time !== "number") {
-
-			time = 300;
-
-		}
 
 		if (state) {
 
@@ -37,11 +21,6 @@ export let debounce = function(func, time) {
 			state = null;
 
 		}, time);
-
-	} else {
-
-		console.error();
-		return false;
 
 	}
 
