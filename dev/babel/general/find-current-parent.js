32c87abaf37f482fbe389d32e6c42088960b94af
WebCuteCat <webcutecat@gmail.com>
2018-01-13 01:00:50 +0300

    find-current-parent ver. 1.0.0

--- dev/babel/general/find-current-parent.js
+++ dev/babel/general/find-current-parent.js
@@ -1,17 +1,5 @@
 // findCurrentParent - ver. 1.0.0
 
-// Description
-// * * * = * * *
-
-// Функция для поиска родителя с определенным классом у элемента;
-
-// Возвращает найденного родителя или false, если родителя с нужным классом нет;
-
-// Принимает в себя элемент у которого нужно искать родителя (elem) и класс родителя по которому будет идти поиск (parentClass).
-
-// * * * = * * *
-// End Description
-
 export let findCurrentParent = function(elem, parentClass) {
 
 	if ((elem && typeof elem === "object") && (parentClass && typeof parentClass === "string")) {
@@ -49,11 +37,6 @@ export let findCurrentParent = function(elem, parentClass) {
 			return false;
 
 		}
-
-	} else {
-
-		console.error();
-		return false;
 
 	}
 
