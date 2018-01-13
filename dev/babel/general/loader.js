4a5a630f2c02fa7b80fe158c06adce254da3e754
WebCuteCat <webcutecat@gmail.com>
2018-01-13 20:23:21 +0300

    loader ver. 1.0.0

--- dev/babel/general/loader.js
+++ dev/babel/general/loader.js
@@ -1,290 +1,134 @@
 import {findElemsClass, findFirstClass} from "./find";
 import {applyClasses} from "./apply-classes";
-import {pageLoad} from "./page-load";
-import {lockClass, hideClass} from "./state-classes";
+import {hideClass} from "./state-classes";
 
-// Description
-// * * * = * * *
+// loader - ver. 1.0.0
 
-// Список классов прелоадера. Можно выбирать как отдельными классами так и объектом с их полным списком.
-
-// * * * = * * *
-// End Description
-
-let loaderBlockClass = "loader";
-let loaderIndicatorClass = loaderBlockClass + "__indicator";
-let pageLoaderBlockClass = "page-loader";
-let pageLoaderIndicatorClass = pageLoaderBlockClass + "__indicator";
-
-let loaderInfo = {
-	local: {
-		blockClass: loaderBlockClass,
-		indicatorClass: loaderIndicatorClass
-	},
-	page: {
-		blockClass: pageLoaderBlockClass,
-		indicatorClass: pageLoaderIndicatorClass
-	}
-};
-
-export {loaderBlockClass, loaderIndicatorClass, pageLoaderBlockClass, pageLoaderIndicatorClass, loaderInfo};
-
-// createLoader - ver. 1.0.0
-
-// Description
-// * * * = * * *
-
-// Функция создания блока с прелоудером.
-
-// Возвращает элемент c прелоудером.
-
-// * * * = * * *
-// End Description
-
-export let createLoader = function(blockClasses, indicatorClasses) {
-
-	let $loaderInfo = {
-		block: {
-			element: null,
-			tag: "div",
-			classes: [loaderInfo.local.blockClass]
+export let loader = {
+	info: {
+		local: {
+			block: "loader",
+			indicator: "loader__indicator"
 		},
-		indicator: {
-			element: null,
-			tag: "div",
-			classes: [loaderInfo.local.indicatorClass]
+		page: {
+			block: "page-loader",
+			indicator: "page-loader__indicator"
 		}
-	};
+	},
+	create(mode, params) {
 
-	let setParams = function() {
+		if (mode && typeof mode === "string") {
 
-		if ((blockClasses && typeof blockClasses === "object") && (indicatorClasses && typeof indicatorClasses === "object")) {
+			let info = {
+				block: {
+					el: null,
+					tag: "div",
+					classes: (params && (params.blockClasses && Array.isArray(params.blockClasses))) ? [params.blockClasses] : [this.info[mode].block]
+				},
+				indicator: {
+					el: null,
+					tag: "div",
+					classes: (params && (params.indicatorClasses && Array.isArray(params.indicatorClasses))) ? [params.indicatorClasses] : [this.info[mode].indicator]
+				}
+			};
 
-			$loaderInfo.block.classes = blockClasses;
-			$loaderInfo.block.indicator = indicatorClasses;
+			let block = info.block;
+			let indicator = info.indicator;
+			let createLoader = function() {
+
+				for (let i in info) {
+
+					info[i].el = document.createElement(info[i].tag);
+					applyClasses(info[i].el, info[i].classes, "add");
+
+				}
+
+				info.block.el.appendChild(info.indicator.el);
+
+			};
+
+			createLoader();
+			return info.block.el;
 
 		}
 
-	};
-	let createLoaderElement = function() {
+	},
+	remove(mode, params) {
 
-		for (let i in $loaderInfo) {
+		if ((mode && typeof mode === "string")) {
 
-			let item = $loaderInfo[i];
+			let info = {
+				class: (params.loaderClass && typeof params.loaderClass) ? params.loaderClass : this.info[mode].block,
+				loaders: null,
+				type: (params.type && typeof params.type === "string") ? params.type : "hide",
+				parent: (params.parent && typeof params.parent === "string") ? params.parent : null,
+				timeout: (params.timeout > 0 && typeof params.timeout === "numver") ? params.timeout : 0
+			};
+			let removeLoader = function() {
 
-			item.element = document.createElement(item.tag);
-			applyClasses(item.element, item.classes, "add");
+				setTimeout(function() {
 
-		}
+					if (info.parent && mode == "local") {
 
-		$loaderInfo.block.element.appendChild($loaderInfo.indicator.element);
+						let parents = findElemsClass(info.parent, document);
 
-	};
+						if (parents) {
 
-	setParams();
-	createLoaderElement();
+							for (let parent of parents) {
 
-	return $loaderInfo.block.element;
+								let loader = findFirstClass(info.class, parent);
 
-};
+								if (loader) {
 
-// removeLoaders - ver. 1.0.0
+									if (info.type == "hide") {
 
-// Description
-// * * * = * * *
+										loader.classList.add(hideClass);
 
-// * * * = * * *
-// End Description
+									} else if (info.type == "remove") {
 
-export let removeLoaders = function(mode, timeout, loadersClass) {
+										parent.removeChild(loader);
 
-	if (mode && typeof mode === "string") {
+									}
 
-		let $loadersInfo = {
-			class: loaderInfo.local.blockClass,
-			elements: null,
-			timeout: 0
-		};
-		let setParams = function() {
+								}
 
-			if (loadersClass && typeof loadersClass === "string") {
+							}
 
-				$loadersInfo.class = loadersClass;
+						}
 
-			}
+					} else if (info.loaders && !info.parent) {
 
-			if (timeout && typeof timeout === "number") {
+						for (let loader of info.loaders) {
 
-				$loadersInfo.timeout = Math.abs(timeout);
+							if (info.type == "hide") {
 
-			}
+								loader.classList.add(hideClass);
 
-		};
-		let removeLoader = function() {
+							} else if (info.type == "remove") {
 
-			setTimeout(function() {
+								let parent = loader.parentNode();
 
-				for (let i = 0; i < $loadersInfo.elements.length; i++) {
+								parent.removeChild(loader);
 
-					let loader = $loadersInfo.elements[i];
-					let firstLoader = $loadersInfo.elements[0];
-
-					if (mode == "hide") {
-
-						applyClasses(loader, [hideClass], "add");
-
-					} else if (mode == "remove") {
-
-						if (firstLoader.remove) {
-
-							firstLoader.remove();
-
-						} else {
-
-							let parent = firstLoader.parentNode;
-
-							parent.removeChild(firstLoader);
+							}
 
 						}
 
 					}
 
-				}
-
-			}, $loadersInfo.timeout);
-
-		};
-
-		setParams();
-		$loadersInfo.elements = findElemsClass($loadersInfo.class, document);
-
-		if ($loadersInfo.elements) {
-
-			pageLoad(removeLoader);
-
-		}
-
-	};
-
-};
-
-// createPageLoader - ver. 1.0.0
-
-// Description
-// * * * = * * *
-
-// * * * = * * *
-// End Description
-
-export let createPageLoader = function(blockClasses, indicatorClasses) {
-
-	let $loaderInfo = {
-		block: {
-			element: null,
-			tag: "div",
-			classes: [loaderInfo.page.blockClass]
-		},
-		indicator: {
-			element: null,
-			tag: "div",
-			classes: [loaderInfo.page.indicatorClass]
-		}
-	};
-
-	let setParams = function() {
-
-		if ((blockClasses && typeof blockClasses === "object") && (indicatorClasses && typeof indicatorClasses === "object")) {
-
-			$loaderInfo.block.classes = blockClasses;
-			$loaderInfo.block.indicator = indicatorClasses;
-
-		}
-
-	};
-	let createLoaderElement = function() {
-
-		for (let i in $loaderInfo) {
-
-			let item = $loaderInfo[i];
-
-			item.element = document.createElement(item.tag);
-			applyClasses(item.element, item.classes, "add");
-
-		}
-
-		$loaderInfo.block.element.appendChild($loaderInfo.indicator.element);
-
-	};
-
-	setParams();
-	createLoaderElement();
-
-	return $loaderInfo.block.element;
-
-};
-
-// removePageLoader - ver. 1.0.0
-
-// Description
-// * * * = * * *
-
-// * * * = * * *
-// End Description
-
-export let removePageLoader = function(mode, timeout, loaderClass) {
-
-	if (mode && typeof mode === "string") {
-
-		let $loaderInfo = {
-			class: loaderInfo.page.blockClass,
-			element: null,
-			timeout: 0
-		};
-		let setParams = function() {
-
-			if (loaderClass && typeof loaderClass === "string") {
-
-				$loaderInfo.class = loaderClass;
+				}, info.timeout);
 
 			}
 
-			if (timeout && typeof timeout === "number") {
+			if (!info.parent) {
 
-				$loaderInfo.timeout = Math.abs(timeout);
-
-			}
-
-		};
-		let removeLoader = function() {
-
-			let loader = $loaderInfo.element;
-
-			if (loader) {
-
-				setInterval(function() {
-
-					document.body.classList.remove(lockClass);
-
-					if (mode == "remove") {
-
-						loader.remove();
-
-					} else if (mode == "hide") {
-
-						loader.classList.add(hideClass);
-
-					}
-
-				}, $loaderInfo.timeout);
+				info.loaders = findElemsClass(info.class, document);
 
 			}
 
-		};
+			removeLoader();
 
-		setParams();
-		$loaderInfo.element = findFirstClass($loaderInfo.class, document);
-		pageLoad(removeLoader);
+		}
 
-	};
-
+	}
 };
