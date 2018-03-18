f7fb4786a4756a319f313e78c6de1e98fc710175
WebCuteCat <webcutecat@gmail.com>
2018-03-18 17:58:10 +0300

    add disabled class on state classes

--- dev/babel/general/state-classes.js
+++ dev/babel/general/state-classes.js
@@ -10,6 +10,7 @@ let hoverClass = "is-hover";
 let hideClass = "is-hide";
 let lockClass = "is-lock";
 let fixClass = "is-fix";
+let disabledClass = "is-disabled";
 
 let stateClasses = {
 	active: activeClass,
@@ -23,7 +24,8 @@ let stateClasses = {
 	hover: hoverClass,
 	hide: hideClass,
 	lock: lockClass,
-	fix: fixClass
+	fix: fixClass,
+	disabled: disabledClass
 };
 
-export {activeClass, successClass, errorClass, onClass, offClass, moveClass, openClass, closeClass, hoverClass, hideClass, lockClass, fixClass, stateClasses};
+export {activeClass, successClass, errorClass, onClass, offClass, moveClass, openClass, closeClass, hoverClass, hideClass, lockClass, fixClass, disabledClass, stateClasses};
